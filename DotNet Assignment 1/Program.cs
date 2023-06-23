8bf29887f3b481b5a405ef723be73cac39437e6f
Hemanth Katte <hemanthss24@gmail.com>
2023-06-23 18:02:02 +0530

    Add project files.

---
+++ DotNet Assignment 1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNet_Assignment_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Enter a number(height) and I'll print you a pyramid of the given height : ");
+			int height = Convert.ToInt32 (Console.ReadLine());
+			printPyramid(height);
+
+		}
+
+		static void printPyramid(int height)
+		{
+			int increment = 1;
+			for(int i =0;i < height;i++)
+			{
+				for(int j =0;j < height - i - 1; j++)
+				{
+					Console.Write(" ");
+				}
+				for(int k = i + increment; k > 0; k--)
+				{
+					Console.Write("*");
+				}
+				increment++;
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
